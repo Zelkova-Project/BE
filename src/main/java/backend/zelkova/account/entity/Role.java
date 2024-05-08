e161659029d0a46d323bd54b5fce2d86ae3feed6
eora21 <eora21@naver.com>
2024-05-08 09:34:13 +0900

    feat: 🎸 사용자의 role 작성
    role이 고도화되는 경우 enum이 아닌 entity로 전환, db에서의 내용 만으로 구동 가능하도록 할 것

---
+++ src/main/java/backend/zelkova/account/entity/Role.java
@@ -0,0 +1,14 @@
+package backend.zelkova.account.entity;
+
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ADMIN;
+
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + this.name();
+    }
+}
