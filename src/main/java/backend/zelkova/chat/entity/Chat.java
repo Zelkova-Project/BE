bc87368299d0de2a0c942de8cf22526fe205996b
eora21 <eora21@naver.com>
2024-05-08 09:57:07 +0900

    feat: 🎸 Chat entity 작성

---
+++ src/main/java/backend/zelkova/chat/entity/Chat.java
@@ -0,0 +1,37 @@
+package backend.zelkova.chat.entity;
+
+import backend.zelkova.account.entity.Account;
+import backend.zelkova.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "chats")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat extends BaseEntity {
+
+    @Id
+    @Column(name = "chat_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "chatroom_id")
+    private Chatroom chatroom;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    private String content;
+}
