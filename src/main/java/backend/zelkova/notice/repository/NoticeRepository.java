745a122c83134f8a4b3d0507b7222bda4388e341
eora21 <eora21@naver.com>
2024-05-14 15:55:39 +0900

    feat: 🎸 Notice Repository 작성

---
+++ src/main/java/backend/zelkova/notice/repository/NoticeRepository.java
@@ -0,0 +1,7 @@
+package backend.zelkova.notice.repository;
+
+import backend.zelkova.notice.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
