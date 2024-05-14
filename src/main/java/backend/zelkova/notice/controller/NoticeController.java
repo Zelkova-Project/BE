e89597a15b1f3e0449f7a26ffb7eec6e2fab0f08
eora21 <eora21@naver.com>
2024-05-14 22:54:12 +0900

    feat: 🎸 (NoticeController) 업데이트 메서드 작성

--- src/main/java/backend/zelkova/notice/controller/NoticeController.java
+++ src/main/java/backend/zelkova/notice/controller/NoticeController.java
@@ -2,6 +2,7 @@ package backend.zelkova.notice.controller;
 
 import backend.zelkova.account.model.AccountDetail;
 import backend.zelkova.notice.dto.request.NoticeRequest;
+import backend.zelkova.notice.dto.request.NoticeUpdateRequest;
 import backend.zelkova.notice.dto.response.NoticePreviewResponse;
 import backend.zelkova.notice.dto.response.NoticeResponse;
 import backend.zelkova.notice.service.NoticeService;
@@ -14,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,6 +46,18 @@ public class NoticeController {
 
         Long noticeId = noticeService.write(accountDetail, noticeRequest.getTitle(), noticeRequest.getContent());
         return ResponseEntity.created(URI.create("/notices/" + noticeId))
+                .build();
+    }
+
+    @PatchMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
+    public ResponseEntity<Void> update(@AuthenticationPrincipal AccountDetail accountDetail,
+                                       @RequestBody @Valid NoticeUpdateRequest noticeUpdateRequest) {
+
+        noticeService.update(accountDetail, noticeUpdateRequest.getNoticeId(), noticeUpdateRequest.getTitle(),
+                noticeUpdateRequest.getContent());
+
+        return ResponseEntity.noContent()
                 .build();
     }
 }
