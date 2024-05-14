53332e7b6ab05fee980bd462931d847975f99ee2
eora21 <eora21@naver.com>
2024-05-14 21:06:00 +0900

    feat: 🎸 (NoticeService) 공지 미리보기 획득 메서드 작성

--- src/main/java/backend/zelkova/notice/service/NoticeService.java
+++ src/main/java/backend/zelkova/notice/service/NoticeService.java
@@ -3,9 +3,14 @@ package backend.zelkova.notice.service;
 import backend.zelkova.account.entity.Account;
 import backend.zelkova.account.model.AccountDetail;
 import backend.zelkova.account.operator.AccountReader;
+import backend.zelkova.notice.dto.response.NoticePreviewResponse;
+import backend.zelkova.notice.dto.response.NoticeResponse;
 import backend.zelkova.notice.entity.Notice;
+import backend.zelkova.notice.operator.NoticeReader;
 import backend.zelkova.notice.operator.NoticeSupplier;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +21,16 @@ public class NoticeService {
 
     private final AccountReader accountReader;
     private final NoticeSupplier noticeSupplier;
+    private final NoticeReader noticeReader;
 
     @Transactional
     public Long write(AccountDetail accountDetail, String title, String content) {
         Account account = accountReader.findAccountById(accountDetail.getAccountId());
         Notice notice = noticeSupplier.supply(account, title, content);
         return notice.getId();
+    }
+
+    public Page<NoticePreviewResponse> getNoticePreviews(Pageable pageable) {
+        return noticeReader.findAll(pageable);
     }
 }
