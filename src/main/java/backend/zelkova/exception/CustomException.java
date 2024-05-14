99fff9c31ca5aa2cb98c360578ff5b919c460a45
eora21 <eora21@naver.com>
2024-05-14 14:47:04 +0900

    feat: 🎸 직접 작성한 예외 클래스 작성

---
+++ src/main/java/backend/zelkova/exception/CustomException.java
@@ -0,0 +1,11 @@
+package backend.zelkova.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ExceptionStatus exceptionStatus;
+}
