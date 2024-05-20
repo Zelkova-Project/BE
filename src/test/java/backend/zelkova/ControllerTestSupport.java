764c3c1c182b78a368138799c6520be1c37071e0
eora21 <eora21@naver.com>
2024-05-20 14:04:45 +0900

    test: 💍 Account 컨트롤러 테스트 작성

--- src/test/java/backend/zelkova/ControllerTestSupport.java
+++ src/test/java/backend/zelkova/ControllerTestSupport.java
@@ -1,15 +1,31 @@
 package backend.zelkova;
 
+import backend.zelkova.account.service.AccountService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.lang.reflect.Constructor;
 import java.util.Map;
 import java.util.Map.Entry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected AccountService accountService;
 
     protected <T> T getInstance(Class<T> clazz, Map<String, Object> params) throws Exception {
         Constructor<T> declaredConstructor = clazz.getDeclaredConstructor();
