e9372927836e6cf021feee20b71883d77de7a91a
eora21 <eora21@naver.com>
2024-06-17 16:45:38 +0900

    fix: 🐛 웹소켓 유저 손쉬운 확인을 위해 HandlerMethodArgumentResolver 작성

--- src/main/java/backend/zelkova/config/WebSocketConfig.java
+++ src/main/java/backend/zelkova/config/WebSocketConfig.java
@@ -1,11 +1,19 @@
 package backend.zelkova.config;
 
+import backend.zelkova.account.model.AccountDetail;
+import backend.zelkova.exception.CustomException;
+import backend.zelkova.exception.ExceptionStatus;
+import java.security.Principal;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -32,11 +40,32 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
+        argumentResolvers.add(new AccountDetailWebsocketArgumentResolver());
     }
 
     @Bean
     HttpSessionHandshakeInterceptor httpSessionHandshakeInterceptor() {
         return new HttpSessionHandshakeInterceptor();
+    }
+
+    static class AccountDetailWebsocketArgumentResolver implements HandlerMethodArgumentResolver {
+        @Override
+        public boolean supportsParameter(MethodParameter parameter) {
+            MethodParameter nestedParameter = parameter.nestedIfOptional();
+            Class<?> paramType = nestedParameter.getNestedParameterType();
+            return AccountDetail.class.isAssignableFrom(paramType);
+        }
+
+        @Override
+        @Nullable
+        public Object resolveArgument(MethodParameter parameter, Message<?> message) {
+            Principal principal = SimpMessageHeaderAccessor.getUser(message.getHeaders());
+
+            if (principal instanceof AbstractAuthenticationToken abstractAuthenticationToken) {
+                return abstractAuthenticationToken.getPrincipal();
+            }
+
+            throw new CustomException(ExceptionStatus.FAIL_CONVERT);
+        }
     }
 }
