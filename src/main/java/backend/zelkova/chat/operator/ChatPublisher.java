cd8fc313791264df5b33be9ac1528b1cfd0e5e40
eora21 <eora21@naver.com>
2024-06-21 10:12:52 +0900

    fix: 🐛 자기 자신에게 보낼 때 예외 발생하던 부분 수정
    Set.of는 중복된 데이터가 있을 시 예외가 발생한다. of 메서드는 프로그래머의 입장에서 잘못된 데이터(중복된 데이터)를
    넣었음을 알린다.

--- src/main/java/backend/zelkova/chat/operator/ChatPublisher.java
+++ src/main/java/backend/zelkova/chat/operator/ChatPublisher.java
@@ -4,6 +4,7 @@ import backend.zelkova.account.entity.Account;
 import backend.zelkova.chat.dto.response.ChatResponse;
 import backend.zelkova.chat.entity.Chat;
 import backend.zelkova.chat.model.MessageDestination;
+import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -21,16 +22,20 @@ public class ChatPublisher {
 
     public void publish(Account sender, Account receiver, String content) {
         Chat chat = chatSupplier.supply(sender, receiver, content);
-        sendMessage(chat.getId(), chat.getContent(), chat.getSender().getId(), chat.getReceiver().getId());
+        sendMessage(chat);
     }
 
-    private void sendMessage(Long chatId, String content, Long... accountIds) {
-        Set.of(accountIds).forEach(
-                accountId -> sendMessageEachAccount(accountId, chatId, content));
+    private void sendMessage(Chat chat) {
+        Long senderId = chat.getSender().getId();
+        Long receiverId = chat.getReceiver().getId();
+
+        ChatResponse chatResponse = new ChatResponse(chat.getId(), senderId, chat.getContent());
+
+        Set.copyOf(List.of(senderId, receiverId))
+                .forEach(accountId -> sendMessageEachAccount(accountId, chatResponse));
     }
 
-    private void sendMessageEachAccount(Long accountId, Long chatId, String content) {
-        simpMessagingTemplate.convertAndSendToUser(accountId.toString(), MessageDestination.MESSAGE,
-                new ChatResponse(chatId, content));
+    private void sendMessageEachAccount(Long receiverId, ChatResponse chatResponse) {
+        simpMessagingTemplate.convertAndSendToUser(receiverId.toString(), MessageDestination.MESSAGE, chatResponse);
     }
 }
