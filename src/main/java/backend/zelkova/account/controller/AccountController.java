dce4379e58e2670baf2856675d366047e28f5737
eora21 <eora21@naver.com>
2024-06-21 19:09:08 +0900

    feat: 🎸 로그인 id 중복 여부 확인 메서드 작성

--- src/main/java/backend/zelkova/account/controller/AccountController.java
+++ src/main/java/backend/zelkova/account/controller/AccountController.java
@@ -2,13 +2,16 @@ package backend.zelkova.account.controller;
 
 
 import backend.zelkova.account.dto.request.SignupRequestDto;
+import backend.zelkova.account.dto.response.PossibleLoginIdResponse;
 import backend.zelkova.account.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final AccountService accountService;
+
+    @GetMapping("/accounts/availability")
+    public ResponseEntity<PossibleLoginIdResponse> isPossibleLoginId(@RequestParam String loginId) {
+        boolean possible = accountService.isPossibleLoginId(loginId);
+        return ResponseEntity.ok(new PossibleLoginIdResponse(possible));
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@RequestBody @Valid SignupRequestDto requestDto) {
