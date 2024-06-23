ddf91ef5609a574da1464f371f4d0b881e64bcf6
eora21 <eora21@naver.com>
2024-06-23 14:20:36 +0900

    fix: 🐛 release, master일 때만 actuator 시큐리티 적용되도록 수정

--- src/main/java/backend/zelkova/config/ActuatorSecurityConfig.java
+++ src/main/java/backend/zelkova/config/ActuatorSecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Profile({"release", "master"})
 public class ActuatorSecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
