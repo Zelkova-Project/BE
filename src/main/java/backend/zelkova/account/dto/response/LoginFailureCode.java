ba99c055fb3ea7452b21d14e9a91ed1bad957bab
eora21 <eora21@naver.com>
2024-06-28 10:59:24 +0900

    feat: 🎸 로그인 시 referer 헤더 검증

--- src/main/java/backend/zelkova/account/dto/response/LoginFailureCode.java
+++ src/main/java/backend/zelkova/account/dto/response/LoginFailureCode.java
@@ -3,5 +3,6 @@ package backend.zelkova.account.dto.response;
 public enum LoginFailureCode {
     NOT_EXIST_LOGIN_ID,
     WRONG_PASSWORD,
+    WRONG_REFERER_HEADER,
     ACCOUNT_PROBLEM;
 }
---
+++ src/main/java/backend/zelkova/config/filter/RefererValidFilter.java
@@ -0,0 +1,35 @@
+package backend.zelkova.config.filter;
+
+import backend.zelkova.account.exception.WrongRefererException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class RefererValidFilter extends OncePerRequestFilter {
+
+    private final String allowReferer;
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if ("/login".equals(request.getRequestURI())) {
+            String referer = request.getHeader("Referer");
+
+            if (Objects.isNull(referer) || !referer.equals(allowReferer)) {
+                authenticationFailureHandler.onAuthenticationFailure(request, response, new WrongRefererException());
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
